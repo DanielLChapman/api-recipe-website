fb114fdab4f14293f2bd1dd88a4c7fd6f00699ae
RustyRadio <danielc@my.ccsu.edu>
2017-03-10 17:13:24 -0500

    finished user routes

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -52,5 +52,50 @@ describe UsersController do
 		end
 	end
 	
+	describe "PUT/PATCH #update" do
+		context "when is successfully updated" do
+			before(:each) do
+				@user = FactoryGirl.create :user
+				patch :update, params: { id: @user.id,
+                         user: { email: "newmail@example.com" } }, format: :json
+			end
+			
+			it "renders the json representation for the updated user" do
+				user_response = JSON.parse(response.body, symbolize_names: true)
+				user_response[:email].should eql "newmail@example.com"
+			end
+			
+			it { should respond_with 200 }
+		end
+		
+		context "when is not updated" do
+			before(:each) do
+				@user = FactoryGirl.create :user
+				patch :update, params: {id: @user.id, user: {email: "newmailbademailexample.com"} }, format: :json
+			end
+			
+			it "renders an errors json" do
+				user_response = JSON.parse(response.body, symbolize_names: true)
+				user_response.should have_key(:errors)
+			end
+			
+			it "renders the json errors on why the user could not be updated" do
+				user_response = JSON.parse(response.body, symbolize_names: true)
+				user_response[:errors][:email].should include "is invalid"
+			end
+			
+			it {should respond_with 422 }
+				
+		end
+	end
+	
+	describe "DELETE #destroy" do
+		before(:each) do
+			@user = FactoryGirl.create :user
+			delete :destroy, { id: @user.id }, format: :json
+		end
+		
+		it {should respond_with 204 }
+	end
 	
 end
