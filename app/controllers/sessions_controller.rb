b71d5917a6b6065f185122e19f4222b14818d8d2
RustyRadio <danielc@my.ccsu.edu>
2017-03-13 17:12:14 -0400

    Adds destroy sessions action added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,10 @@ class SessionsController < ApplicationController
 		end
 	end
 	
+	def destroy
+		user = User.find_by(auth_token: params[:id])
+		user.generate_authentication_token!
+		user.save
+		head 204
+	end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -36,4 +36,14 @@ RSpec.describe SessionsController, type: :controller do
 		end
 	
 	end
+	
+	describe "DELETE #destroy" do
+		before(:each) do
+			@user = FactoryGirl.create :user
+			sign_in @user, store: false
+			delete :destroy, id: @user.auth_token
+		end
+		
+		it { should respond_with 204 }
+	end
 end
