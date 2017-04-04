6a4299639c8fa13aa1f4fcb0000f50c2a76d2c1c
RustyRadio <danielc@my.ccsu.edu>
2017-04-04 09:24:58 -0400

    Re-add in old routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users, :skip => [:registrations] 
-  	as :user do
-  		get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-		put 'users' => 'devise/registrations#update', :as => 'user_registration'
-  end
+  devise_for :users
+	#, :skip => [:registrations] 
+  	#as :user do
+  #		get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+#		put 'users' => 'devise/registrations#update', :as => 'user_registration'
+ # end
 	
 	root 'pages#show'
 	
 	get "pages/show"
 	get "pages/admin"
 	
-	devise_scope :user do
-		get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-		get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
-	  end
+	#devise_scope :user do
+	#	get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+	#	get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to 	#sign_up/registration
+	#  end
 	resources :users, :only => [:show, :create, :update, :destroy]
 	resources :sessions, :only => [:create, :destroy]
 	resources :recipes, :only => [:edit, :show, :index, :create, :update, :destroy, :new] do
